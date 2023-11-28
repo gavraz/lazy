a6447ba14a4f2e8daef5faa38f61d4253fe0d7e9
Elad Gavra <elad@cyolo.io>
2023-11-28 23:00:00 +0100

    Rename

---
+++ easy.go
@@ -0,0 +1,20 @@
+package lazy
+
+type Easy[T any] struct {
+	v T
+	f Iterator[T]
+}
+
+func (ez *Easy[T]) Next() bool {
+	v, ok := ez.f.f()
+	if !ok {
+		return false
+	}
+
+	ez.v = v
+	return true
+}
+
+func (ez *Easy[T]) Value() T {
+	return ez.v
+}
